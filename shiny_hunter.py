e204e761a8789d14549c7b099dcd0ff59f0f51d4
Taxalottle Syndicate <bthole41@gmail.com>
2026-07-03 12:28:57 -0400

    forgot a bit
    I forgot this line

--- shiny_hunter.py
+++ shiny_hunter.py
@@ -3,6 +3,8 @@ import tkinter as tk
 root = tk.Tk()
 root.title("Shiny Hunter")
 root.geometry("1280x720")
+
+root.attributes('-fullscreen', True)
 root.bind('<Escape>', lambda e: root.attributes('-fullscreen', False))
 
 root.mainloop()
