9d8c7e14c6996219ada111848fd46a8c673c4fea
Taxalottle Syndicate <bthole41@gmail.com>
2026-07-03 12:37:48 -0400

    Sword/shield button
    Puttin in some buttons to see how they work.

--- shiny_hunter.py
+++ shiny_hunter.py
@@ -1,5 +1,9 @@
 import tkinter as tk
 
+#Button scripts
+
+def on_click_sword_shield():
+    print("Sword and Shield button clicked")
 
 #Initialize the main window
 root = tk.Tk()
@@ -13,7 +17,10 @@ root.configure(bg="#4a4a4a")
 
 #Populate the main window with widgets
 
-label = tk.Label(root, text="Select which game you will be hunting in:", font=("Arial", 24))
+label = tk.Label(root, text="Select which game you will be hunting in:", font=("Arial", 24), bg="#4a4a4a", fg="black")
 label.pack(pady=20)
+
+button = tk.Button(root, text="Sword and Shield", bg="#4a4a4a", fg="black" font=("Arial", 18), command=on_click_sword_shield)
+button.pack(pady=50)
 
 root.mainloop()
