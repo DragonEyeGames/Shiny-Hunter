79cd2b0b17209ffa4f0ffc1c01d6986e21d78303
Taxalottle Syndicate <bthole41@gmail.com>
2026-07-03 17:35:55 -0400

    New pokemon
    I put in some more pokemon.

---
+++ capture_card.py
@@ -0,0 +1,5 @@
+import tkinter as tk
+
+class SwitchScreen(tk.Frame):
+    def __init__(self, parent, back_callback):
+        super().__init__(parent, bg="#4a4a4a")
--- lg.py
+++ lg.py
@@ -4,6 +4,9 @@ class LGScreen(tk.Frame):
     def __init__(self, parent, back_callback):
         super().__init__(parent, bg="#4a4a4a")
 
+
+
+        #Game title
         label = tk.Label(
             self,
             text="Let's go Eevee and Pikachu",
@@ -12,17 +15,43 @@ class LGScreen(tk.Frame):
         )
         label.pack(pady=20)
 
+        #Text for the user to select a pokemon
         label = tk.Label(
             self,
             text="Select the pokemon you want to hunt:",
             font=("Arial", 24),
             bg="#4a4a4a"
         )
-        label.pack(pady=20)
+        label.place(x=70, y=80)
+
+        #Back button
 
         back_button = tk.Button(
             self,
             text="Back",
+            font=("Arial", 20),
             command=back_callback
         )
-        back_button.pack()
+        back_button.place(x=340, y=420, width=100, height=40)
+
+    #Pokemon creation script
+    def create_pokemon_button(self, x, y, image, name):
+        button = tk.Button(
+            self,
+            image=image,
+            bg="#8f8d8d",
+            activebackground="#bfbfbf",
+            borderwidth=0
+        )
+        button.place(x=x, y=y, width=74, height=74)
+
+        label = tk.Label(
+            self,
+            text=name,
+            font=("Arial", 10),
+            fg="black",
+            bg="white"
+        )
+        label.place(x=x+4, y=y + 54, width=66, height=16)
+
+        return button, label
