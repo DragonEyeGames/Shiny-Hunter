6fffb140512c2e83197c90e8cf286573b784da5e
Taxalottle Syndicate <bthole41@gmail.com>
2026-07-03 13:28:53 -0400

    More buttons
    I put in some more buttons to look at.

--- shiny_hunter.py
+++ shiny_hunter.py
@@ -5,6 +5,16 @@ import tkinter as tk
 def on_click_sword_shield():
     print("Sword and Shield button clicked")
 
+def on_click_lets_go():
+    print("Let's go button clicked")
+
+def on_click_diamond_pearl():
+    print("Diamond and Pearl button clicked")
+
+def on_click_red_green():
+    print("Red and Green button clicked")
+
+
 #Initialize the main window
 root = tk.Tk()
 root.title("Shiny Hunter")
@@ -20,7 +30,16 @@ root.configure(bg="#4a4a4a")
 label = tk.Label(root, text="Select which game you will be hunting in:", font=("Arial", 24), bg="#4a4a4a", fg="black")
 label.pack(pady=20)
 
-button = tk.Button(root, text="SW/SH", bg="#4a4a4a", fg="black", font=("Arial", 18), command=on_click_sword_shield)
-button.place(x=50, y=70, width=100, height=100)
+lets_go_button = tk.Button(root, text="Let's Go", bg="#bfbfbf", fg="black", font=("Arial", 18), command=on_click_lets_go)
+lets_go_button.place(x=50, y=70, width=100, height=100)
+
+sword_shield_button = tk.Button(root, text="SW/SH", bg="#bfbfbf", fg="black", font=("Arial", 18), command=on_click_sword_shield)
+sword_shield_button.place(x=200, y=70, width=100, height=100)
+
+diamond_pearl_button = tk.Button(root, text="Bd/Sp", bg="#bfbfbf", fg="black", font=("Arial", 18), command=on_click_diamond_pearl)
+diamond_pearl_button.place(x=350, y=70, width=100, height=100)
+
+red_green_button = tk.Button(root, text="Fr/Lg", bg="#bfbfbf", fg="black", font=("Arial", 18), command=on_click_red_green)
+red_green_button.place(x=500, y=70, width=100, height=100)
 
 root.mainloop()
