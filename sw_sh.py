161f8ce75c347642ae66921a99bcd583d2f3d25a
Taxalottle Syndicate <bthole41@gmail.com>
2026-07-03 14:13:55 -0400

    formatting+regice
    I formatted the screen a little better and put in regice as well.

--- sw_sh.py
+++ sw_sh.py
@@ -5,7 +5,7 @@ class SwShScreen(tk.Frame):
         super().__init__(parent, bg="#4a4a4a")
 
         self.regirock = tk.PhotoImage(file="images/regirock.png")
-
+        self.regice = tk.PhotoImage(file="images/regice.png")
 
         #Game title
         label = tk.Label(
@@ -25,14 +25,48 @@ class SwShScreen(tk.Frame):
         )
         label.place(x=70, y=80)
 
+        #Regirock button and text
+
         regirock_button = tk.Button(
             self,
             image=self.regirock,
-            bg="#bfbfbf",
-            activebackground="#d0d0d0",
-            borderwidth=2
+            bg="#8f8d8d",
+            activebackground="#bfbfbf",
+            borderwidth=0
         )
         regirock_button.place(x=50, y=150)
+
+        regirock_text = tk.Label(
+            self,
+            text="Regirock",
+            font=("Arial", 10),
+            fg="black",
+            bg="white"
+        )
+        regirock_text.place(x=50, y=200, width=70, height=20)
+
+        #Regice button and text
+
+        regice_button = tk.Button(
+            self,
+            image=self.regirock,
+            bg="#8f8d8d",
+            activebackground="#bfbfbf",
+            borderwidth=0
+        )
+        regice_button.place(x=150, y=150)
+
+        regice_text = tk.Label(
+            self,
+            text="Regice",
+            font=("Arial", 10),
+            fg="black",
+            bg="white"
+        )
+        regice_text.place(x=150, y=200, width=70, height=20)
+
+        #Back button
+
         back_button = tk.Button(
             self,
             text="Back",
