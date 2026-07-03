cb38d97a060a17f301436e463550b295c2b3f0ae
Taxalottle Syndicate <bthole41@gmail.com>
2026-07-03 12:33:31 -0400

    text
    I put it some text and comments. Will see how this looks on the pi.

--- shiny_hunter.py
+++ shiny_hunter.py
@@ -1,10 +1,19 @@
 import tkinter as tk
 
+
+#Initialize the main window
 root = tk.Tk()
 root.title("Shiny Hunter")
 root.geometry("1280x720")
 
 root.attributes('-fullscreen', True)
 root.bind('<Escape>', lambda e: root.attributes('-fullscreen', False))
+
+root.configure(bg="#4a4a4a")
+
+#Populate the main window with widgets
+
+label = tk.Label(root, text="Select which game you will be hunting in:", font=("Arial", 24))
+label.pack(pady=20)
 
 root.mainloop()
