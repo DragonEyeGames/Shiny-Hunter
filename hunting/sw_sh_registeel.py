992c40597ae524fbfff59c0a68700e663029ed94
Taxalottle Syndicate <bthole41@gmail.com>
2026-07-04 15:16:29 -0400

    Speeding up
    The old one would skip a shiny

--- hunting/sw_sh_registeel.py
+++ hunting/sw_sh_registeel.py
@@ -2,11 +2,11 @@ commands = [
     ("a", 0.5),
     ("a", 0.8),
     ("a", 0.2),
-    ("a", 0.2),
-    ("a", 0.2),
-    ("a", 0.2),
-    ("a", 0.2),
-    ("a", 0.2),
+    ("a", 0.1),
+    ("a", 0.1),
+    ("a", 0.1),
+    ("a", 0.1),
+    ("a", 0.1),
     ("a", 13), #14 seconds to wait for the menu to appear, rought 15.7 seconds if shiny. We will search for 1 second to be safe.
     ("search", 1), #Check to see if shiny
 
