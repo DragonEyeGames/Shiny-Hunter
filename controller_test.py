62f27d2508f93eb5f573a1e99e38fb37f3ab1da7
Taxalottle Syndicate <bthole41@gmail.com>
2026-07-04 10:11:26 -0400

    I forgot more syntax

--- controller_test.py
+++ controller_test.py
@@ -1,11 +1,7 @@
 from nxbt import Nxbt, PRO_CONTROLLER, Buttons
 
 nx = Nxbt()
-
-index = nx.create_controller(nx.PRO_CONTROLLER)
-
+index = nx.create_controller(PRO_CONTROLLER)
 print("Go to Change Grip/Order screen")
-
 nx.wait_for_connection(index)
-
 print("CONNECTED")
