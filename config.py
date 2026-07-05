9d63fc9d834bbb775bc0ac9e8b620fdb703d21e7
Taxalottle Syndicate <bthole41@gmail.com>
2026-07-05 19:37:40 -0400

    spelling fix

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ status="Idle"
 hunting_data = { "Registeel": {"resets": 0, "time_spent": 0.000}, "(loading)": {"resets": 0, "time_spent": 0.000} }
 
 
-curret_reset_time=0.000
+current_reset_time=0.000
 last_reset_time=0.000
 
 roi = {
