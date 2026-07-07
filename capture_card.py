507899b1648a687e25aaf3fb3c1ca9f9833a8310
Taxalottle Syndicate <bthole41@gmail.com>
2026-07-07 14:38:34 -0400

    Forgot the flow of my code

--- capture_card.py
+++ capture_card.py
@@ -25,10 +25,6 @@ class CaptureCard(tk.Frame):
         self.start_time = time.time()
         self.initialize_time = self.start_time
 
-        config.hunting_data = load_data(config.hunting_data)
-        if(not config.pokemon_name in config.hunting_data):
-            config.hunting_data[config.pokemon_name]={"resets": 0, "time_spent": 0.000}
-
         def end_hunt():
             print("ending hunt")
             config.status="Ending Hunt"
@@ -72,6 +68,8 @@ class CaptureCard(tk.Frame):
 
         self.reset_time_label = tk.Label(self, bg="#5e5e5e", fg="white", font=("C052", 16), text="Average Time/Reset: Loading")
         self.reset_time_label.place(x=340, y=280)
+
+        config.hunting_data = load_data(config.hunting_data)
 
         self.label.lift()
         self.update_frame()
@@ -131,6 +129,8 @@ class CaptureCard(tk.Frame):
             self.callback()
 
     def update_frame(self):
+        if(not config.pokemon_name in config.hunting_data):
+            config.hunting_data[config.pokemon_name]={"resets": 0, "time_spent": 0.000}
         if(self.initialize_time!=self.start_time and config.status!="Shiny Detected!" and config.status != "Ending Hunt"):
             config.hunting_data[config.pokemon_name]['time_spent']+=(time.time()-self.start_time)
             config.current_reset_time+=(time.time()-self.start_time)
