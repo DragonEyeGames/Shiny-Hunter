543d1e5227d63b84fa53392bee6ce4e863c30f6b
Taxalottle Syndicate <bthole41@gmail.com>
2026-07-10 13:27:08 -0400

    Removing Unneded files + requirements
    I put in requirements and removed unneded files

--- controller_test.py
+++
@@ -1,7 +0,0 @@
-from nxbt import Nxbt, PRO_CONTROLLER, Buttons
-
-nx = Nxbt()
-index = nx.create_controller(PRO_CONTROLLER)
-print("Go to Change Grip/Order screen")
-nx.wait_for_connection(index)
-print("CONNECTED")
