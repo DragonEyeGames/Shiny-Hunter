cf195e63f757947564c69fac7f9eed6d09cd80a0
Taxalottle Syndicate <bthole41@gmail.com>
2026-07-12 21:33:17 -0400

    Timing improvements

--- hunting/bd_sp_arceus.py
+++ hunting/bd_sp_arceus.py
@@ -2,7 +2,7 @@ commands = [
     ("up", 14.5),
     ("a", 1),
     ("bd_sp_a", 10),#21.5),
-    ("wait", 6.2),
-    ("bd-sp_search", .8), #Check to see if shiny
+    ("wait", 6),
+    ("bd-sp_search", .5), #Check to see if shiny
 
 ]
