04a5383025c20361d6803dabd76b647686afcb5e
Taxalottle Syndicate <bthole41@gmail.com>
2026-07-13 13:29:22 -0400

    Button centering
    Putting the button in the middle of the screen, and lowering them some.

--- main.py
+++ main.py
@@ -111,6 +111,8 @@ def create_game_button(parent, x, y, image, hover_image, command):
         border_color="black",
         border_width=3,
         corner_radius=12,
+        border_spacing=0,
+        compound="center",
         command=command
     )
 
@@ -165,21 +167,21 @@ subtitle.pack()
 
 #Create the game buttons with a consistent theme in mind.
 
-create_game_button(main_menu, 100, 120, eevee, selected_eevee, on_click_lets_go_eevee)
+create_game_button(main_menu, 100, 130, eevee, selected_eevee, on_click_lets_go_eevee)
 
-create_game_button(main_menu, 100, 240, pikachu, selected_pikachu, on_click_lets_go_pikachu)
+create_game_button(main_menu, 100, 250, pikachu, selected_pikachu, on_click_lets_go_pikachu)
 
-create_game_button(main_menu, 250, 120, sword, selected_sword, on_click_sword)
+create_game_button(main_menu, 250, 130, sword, selected_sword, on_click_sword)
 
-create_game_button(main_menu, 250, 240, shield, selected_shield, on_click_shield)
+create_game_button(main_menu, 250, 250, shield, selected_shield, on_click_shield)
 
-create_game_button(main_menu, 400, 120, brilliant_diamond, selected_brilliant_diamond, on_click_brilliant_diamond)
+create_game_button(main_menu, 400, 130, brilliant_diamond, selected_brilliant_diamond, on_click_brilliant_diamond)
 
-create_game_button(main_menu, 400, 240, shining_pearl, selected_shining_pearl, on_click_shining_pearl)
+create_game_button(main_menu, 400, 250, shining_pearl, selected_shining_pearl, on_click_shining_pearl)
 
-create_game_button(main_menu, 550, 120, fire_red, selected_fire_red, on_click_fire_red)
+create_game_button(main_menu, 550, 130, fire_red, selected_fire_red, on_click_fire_red)
 
-create_game_button(main_menu, 550, 240, leaf_green, selected_leaf_green, on_click_leaf_green)
+create_game_button(main_menu, 550, 250, leaf_green, selected_leaf_green, on_click_leaf_green)
 
 #The button to close down the program
 end_button = ctk.CTkButton(main_menu,text="Quit Program",width=230,height=40,fg_color="#C0392B",hover_color="#96281B",font=("Arial",16,"bold"),command=close_project)
