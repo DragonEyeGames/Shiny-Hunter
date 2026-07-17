dbb492f6b1b9fae06a778ac0ed0abcd7bae6568c
Taxalottle Syndicate <bthole41@gmail.com>
2026-07-17 09:05:33 -0400

    Forgot a time

--- hunting/bd_sp_arceus.py
+++ hunting/bd_sp_arceus.py
@@ -1,7 +1,7 @@
 commands = [
     ("up", 14.5),
     ("a", 1),
-    ("white_skip"),
+    ("white_skip", 0),
     ("white_a", 10),#21.5),
     ("wait", 5.9),
     ("search", .5), #Check to see if shiny
