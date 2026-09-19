84dc9f6a975b63352b74fc11f40ddd26b617842c
Taxalottle Syndicate <bthole41@gmail.com>
2026-09-19 14:50:27 -0400

    Forgot ""

--- screens/sword.py
+++ screens/sword.py
@@ -91,7 +91,7 @@ class SwScreen(ctk.CTkFrame):
     def create_pokemon_button(self, x, y, image, name, boot_screen, disabled=False):
 
         # Button (80x80 size, placed at x+10, y+10)
-        button = ctk.CTkButton(self, image=image, text="", width=90, height=90, border_spacing=0, border_width=3, border_color=black, corner_radius=10, fg_color="#5e5e5e", bg_color="#21344a", hover_color="#bfbfbf", command=lambda: self.start_hunt(name,boot_screen))
+        button = ctk.CTkButton(self, image=image, text="", width=90, height=90, border_spacing=0, border_width=3, border_color="black", corner_radius=10, fg_color="#5e5e5e", bg_color="#21344a", hover_color="#bfbfbf", command=lambda: self.start_hunt(name,boot_screen))
         button.place(x=x+5, y=y+5)
 
         if disabled:
