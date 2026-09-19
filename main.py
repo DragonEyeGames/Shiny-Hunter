d8c6a63165e37f205f0cd148aaa88d8204fe8913
Taxalottle Syndicate <bthole41@gmail.com>
2026-09-19 10:59:20 -0400

    This hsould do it

--- main.py
+++ main.py
@@ -199,12 +199,12 @@ selected_fire_red = ctk.CTkImage(light_image=Image.open("logos/selected_fire_red
 leaf_green = ctk.CTkImage(light_image=Image.open("logos/leaf_green.png"),dark_image=Image.open("logos/leaf_green.png"),size=(100,100))
 selected_leaf_green = ctk.CTkImage(light_image=Image.open("logos/selected_leaf_green.png"),dark_image=Image.open("logos/selected_leaf_green.png"),size=(100,100))
 
-ctk.FontManager.load_font("fonts\Risque-Regular.ttf")
+ctk.FontManager.load_font(r"fonts\Risque-Regular.ttf")
 
 title = ctk.CTkLabel(
     main_menu,
     text="Pokémon Shiny Hunter!",
-    font=("Risque",40,"bold"),
+    font=("Risque",40),
     text_color="#2b89d9"
 )
 
