1074027438b99f89b39ff263ab04e770b8b2b7c0
Taxalottle Syndicate <bthole41@gmail.com>
2026-09-19 17:14:27 -0400

    'Tis Buggin

--- main.py
+++ main.py
@@ -91,8 +91,6 @@ def open_manual_control():
 
 def show_main_menu():
 
-    os.execv(sys.executable, [sys.executable] + sys.argv)
-
     config.pokemon_name=""
     config.game_name=""
     config.start_camera=False
