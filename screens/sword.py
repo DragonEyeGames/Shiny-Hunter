e39ac2cfe6f2cea8ff6dacd774e85bf6c03b4a8a
Taxalottle Syndicate <bthole41@gmail.com>
2026-09-19 14:24:06 -0400

    Image resizing

--- screens/sword.py
+++ screens/sword.py
@@ -11,7 +11,7 @@ class SwScreen(ctk.CTkFrame):
 
         # Images
 
-        self.size=(75, 75)
+        self.size=(80, 80)
 
         self.regirock = ctk.CTkImage(light_image=Image.open("pokemon/regirock.png"),dark_image=Image.open("pokemon/regirock.png"),size=self.size)
         self.registeel = ctk.CTkImage(light_image=Image.open("pokemon/registeel.png"),dark_image=Image.open("pokemon/registeel.png"),size=self.size)
