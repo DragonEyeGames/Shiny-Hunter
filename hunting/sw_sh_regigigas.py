d0e287c6d160157c0f52316c9a7f36ea859abdad
Taxalottle Syndicate <bthole41@gmail.com>
2026-09-22 19:29:24 -0400

    Slight timing change

--- hunting/sw_sh_regigigas.py
+++ hunting/sw_sh_regigigas.py
@@ -1,9 +1,10 @@
 commands = [
-    ("a", 3.5),
+    ("a", 3),
     ("a", 0.2),
     ("a", 0.2),
     ("a", 0.2),
-    ("black_a", 27), #14 seconds to wait for the menu to appear, rought 15.7 seconds if shiny. We will search for 1 second to be safe.
+    ("black_a", 10), #14 seconds to wait for the menu to appear, rought 15.7 seconds if shiny. We will search for 1 second to be safe.
+    ("wait", 17.1),
     ("search", .6), #Check to see if shiny
 
 ]
