e435db8ded2ce8ffe21c928c44e9f424160b689a
Taxalottle Syndicate <bthole41@gmail.com>
2026-09-22 19:26:57 -0400

    This man is really slow

--- hunting/sw_sh_regigigas.py
+++ hunting/sw_sh_regigigas.py
@@ -3,7 +3,7 @@ commands = [
     ("a", 0.2),
     ("a", 0.2),
     ("a", 0.2),
-    ("black_a", 20), #14 seconds to wait for the menu to appear, rought 15.7 seconds if shiny. We will search for 1 second to be safe.
+    ("black_a", 27), #14 seconds to wait for the menu to appear, rought 15.7 seconds if shiny. We will search for 1 second to be safe.
     ("search", .6), #Check to see if shiny
 
 ]
